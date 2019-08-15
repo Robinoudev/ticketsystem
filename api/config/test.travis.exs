c851529b3d6e08107414a177cbe5205fff648077
Robin Oude Vrielink <robinoudev@gmail.com>
2019-08-15 12:49:07 +0200

    Travis fix5?

--- api/config/test.travis.exs
+++ api/config/test.travis.exs
@@ -6,7 +6,7 @@ config :ticketsystem, Ticketsystem.Repo,
   password: "postgres",
   database: "travis_test_db",
   hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox,
+  pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
