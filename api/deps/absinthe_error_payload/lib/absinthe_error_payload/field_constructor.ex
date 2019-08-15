9a5dd68f9fb3ef91b16c8fdc4a7798f3895b5d6b
Robin Oude Vrielink <robinoudev@gmail.com>
2019-08-15 12:06:47 +0200

    Removed build and deps -.-

--- api/deps/absinthe_error_payload/lib/absinthe_error_payload/field_constructor.ex
+++
@@ -1,8 +0,0 @@
-defmodule AbsintheErrorPayload.FieldConstructor do
-  @callback error(String.t(), String.t(), list()) :: String.t()
-
-  def error(parent_field, field, options \\ [])
-  def error(parent_field, nil, _options), do: parent_field
-  def error(parent_field, field, index: index), do: "#{parent_field}.#{index}.#{field}"
-  def error(parent_field, field, _options), do: "#{parent_field}.#{field}"
-end
