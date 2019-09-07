c25f67471c6cc3cca8f81c448c27fea6a145de02
Robin Oude Vrielink <robinoudev@gmail.com>
2019-09-07 10:41:16 +0200

    Added ticket to seeds

--- api/priv/repo/seeds.exs
+++ api/priv/repo/seeds.exs
@@ -15,26 +15,50 @@ alias Ticketsystem.Accounts.User
 alias Ticketsystem.Companies.Company
 alias Ticketsystem.Companies
 alias Ticketsystem.Repo
+alias Ticketsystem.Tickets
+alias Ticketsystem.Tickets.Ticket
 
 # Remove existing data
+Ticket |> Repo.delete_all()
 User |> Repo.delete_all()
 Company |> Repo.delete_all()
 
+# Companies
 {:ok, company1} = Companies.insert_or_update_company(%{name: "company1"})
 {:ok, company2} = Companies.insert_or_update_company(%{name: "company2"})
 
-Accounts.create_user(%{
-  email: "email1@email.com",
-  name: "name1",
-  username: "username1",
-  password: "password",
-  company_id: company1.id
-})
+# Users
+{:ok, user1} =
+  Accounts.create_user(%{
+    email: "email1@email.com",
+    name: "name1",
+    username: "username1",
+    password: "password",
+    company_id: company1.id
+  })
 
-Accounts.create_user(%{
-  email: "email2@email.com",
-  name: "name2",
-  username: "username2",
-  password: "password",
-  company_id: company2.id
-})
+{:ok, user2} =
+  Accounts.create_user(%{
+    email: "email2@email.com",
+    name: "name2",
+    username: "username2",
+    password: "password",
+    company_id: company2.id
+  })
+
+# Issued Tickets
+Tickets.insert_or_update_ticket(
+  %{
+    title: "First ticket",
+    description: "Description"
+  },
+  user1
+)
+
+Tickets.insert_or_update_ticket(
+  %{
+    title: "Second ticket",
+    description: "Description"
+  },
+  user2
+)
