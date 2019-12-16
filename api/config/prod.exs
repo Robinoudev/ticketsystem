cdb1709ea82b01b0bdc893f37ce4c479533c5a1c
Robin Oude Vrielink <robinoudev@gmail.com>
2019-12-16 09:09:38 +0100

    Fix heroku failure

--- api/config/prod.exs
+++ api/config/prod.exs
@@ -10,6 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :ticketsystem, TicketsystemWeb.Endpoint,
+  http: [port: {:system, "PORT"}],
   url: [host: "ticketsystem-backend-acc", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
